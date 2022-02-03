cfa5cfe9e48f4a7161e9e628a4d7705813731c99
berti <berti@CLASSES.LOCAL>
2022-02-03 13:09:51 +0100

    bootstrap admin

--- admin/index.php
+++ admin/index.php
@@ -4,20 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <h1>Administration</h1>
-    <form action="index.php" method="POST">
-        <div>
-            <input type="text" id="login" name="login" placeholder="Votre login">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 offset-3">
+            <h1>Administration</h1>
+                <form action="index.php" method="POST">
+                    <div class="form-group mt-3">
+                        <input type="text" id="login" name="login" placeholder="Votre login" class="form-control">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="password" name="password" id="password" placeholder="Votre mot de passe" class="form-control">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="submit" class="btn btn-primary" value="Connexion">
+                    </div>
+                </form>
+            </div>
         </div>
-        <div>
-            <input type="password" name="password" id="password" placeholder="Votre mot de passe">
-        </div>
-        <div>
-            <input type="submit" value="Connexion">
-        </div>
-    </form>
+    </div>
+    
 </body>
 </html>
