96cf0b62c67347ba82fb913ccd643b578c8f3754
berti <berti@CLASSES.LOCAL>
2022-02-10 15:24:07 +0100

    formulaire d'ajout

--- admin/productAdd.php
+++ admin/productAdd.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    // système de protection 
     if(!isset($_SESSION['login']))
     {
         header("LOCATION:index.php");
@@ -19,7 +20,27 @@
     <main>
         <div class="container">
             <h1>Ajouter un produit</h1>
-
+            <form action="treatmentAddProduct.php" method="POST">
+                <div class="my-3">
+                    <label for="title">Titre</label>
+                    <input type="text" id="title" name="title" value="" class="form-control">
+                </div>
+                <div class="my-3">
+                    <label for="date">Date</label>
+                    <input type="date" name="date" id="date" value="" class="form-control">
+                </div>
+                <div class="my-3">
+                    <label for="descri">Description</label>
+                    <textarea name="description" id="descri" class="form-control"></textarea>
+                </div>
+                <div class="my-3">
+                    <label for="price">Prix</label>
+                    <input type="number" step="0.01" id="price" name="price" value="" class="form-control">
+                </div>
+                <div class="my-3">
+                    <input type="submit" value="Enregistrer" class="btn btn-success">
+                </div>
+            </form>
         </div>
     </main>
 </body>
